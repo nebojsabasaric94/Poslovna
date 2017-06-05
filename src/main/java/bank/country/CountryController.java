7288baf581b7e1c00edeed37d9064c7faf0f774a
nebojsabasaric94 <nebojsabasaric94@gmail.com>
2017-06-05 04:39:45 +0200

    Prikazivanje izvestaja uradjeno, treba da se sredi da se ne pravi nova konekcija sa bazom vec da se koristi vec postojeca

--- src/main/java/bank/country/CountryController.java
+++ src/main/java/bank/country/CountryController.java
@@ -1,10 +1,14 @@
 package bank.country;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
-import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ResourceNotFoundException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,15 +18,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 
 @RestController
 @RequestMapping("/country")
 public class CountryController {
 
 	private final CountryService countryService;
+	
+	@Autowired
+    private ApplicationContext appContext;
 	
 	@Autowired
 	public CountryController(final CountryService service) {
@@ -58,6 +72,64 @@ public class CountryController {
 	@PutMapping("/update")
 	public  Country update(@RequestBody Country country) {
 		return countryService.save(country);
+	}
+	
+	@RequestMapping(value = "/pdf", method = RequestMethod.GET)
+	@ResponseBody 
+	public void getFile(HttpServletResponse response) {
+		 try {           
+			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bank", "root", "root");
+		       
+			JasperReport jasperReport = JasperCompileManager.compileReport("report1.jrxml");
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection); 
+			JasperExportManager.exportReportToPdfFile(jasperPrint, "D://Test.pdf");
+			
+			
+			
+			 ByteArrayOutputStream out = new ByteArrayOutputStream();
+			    try {
+			        JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+			    } catch (JRException e1) {
+			        e1.printStackTrace();
+			    }
+			    byte[] data = out.toByteArray();
+			    response.setContentType("application/pdf");
+			    response.setContentLength(data.length);
+			    response.getOutputStream().write(data);
+		        response.getOutputStream().flush();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void sendPdfResponse(HttpServletResponse response, JasperPrint jasperPrint, String fileName){
+
+	    //Remove all whitespace from file name
+	    fileName.replaceAll("\\s","");
+
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+	    try {
+	        JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+	    } catch (JRException e1) {
+	        e1.printStackTrace();
+	    }
+
+	    byte[] data = out.toByteArray();
+
+	    response.setContentType("application/pdf");
+	    //To make it a download change "inline" to "attachment"
+	    response.setHeader("Content-disposition", "inline; filename=" + fileName + ".pdf");
+	    response.setContentLength(data.length);
+
+	    try {
+	        response.getOutputStream().write(data);
+	        response.getOutputStream().flush();
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
 	}
 	
 }
